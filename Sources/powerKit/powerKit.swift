a15ab0508d089a69cbacda375ad0e38fcf1b036e
Miles Wirht <regime_skylark.0y@icloud.com>
2025-04-08 15:01:05 -0400

    Error handling for main cases

--- Sources/powerKit/powerKit.swift
+++ Sources/powerKit/powerKit.swift
@@ -43,3 +43,17 @@ public actor CoffeeKit {
 		}
 	}
 
+	public enum CaffeinationError: Error, Sendable {
+		case assertionCreationFailed(type: AssertionType, status: kern_return_t)
+		case userActivityDeclarationFailed(status: kern_return_t)
+		case kqueueCreationFailed(error: String)
+		case kqueueEventRegistrationFailed(error: String)
+		case kqueueMonitorStartFailed
+		case pipeCreationFailed(error: String)
+		case fcntlFailed(error: String)
+		case processWatchNotSupported
+		case alreadyActive
+		case processNotFound(pid: pid_t)
+		case invalidPID(pid: pid_t)
+	}
+
