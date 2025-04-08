82475b407e506dacc0d95e4d35e5f0aa4d352040
Miles Wirht <regime_skylark.0y@icloud.com>
2025-04-08 15:00:25 -0400

    CoffeeKit Actor and Basic Enums
    Focusing on maintainability and debug rn

--- Sources/powerKit/powerKit.swift
+++ Sources/powerKit/powerKit.swift
@@ -3,3 +3,43 @@ import Foundation
 import IOKit.pwr_mgt  // For the vital Power Management headers
 import Logging
 
+// MARK: - coffeeKit Actor
+
+public actor CoffeeKit {
+	// MARK: - Public Enums
+
+	// This enum defines the type of power assertion to create. Conforms to Sendable.
+	public enum AssertionType: CaseIterable, Sendable {
+		case preventDisplaySleep
+		case preventSystemIdleSleep
+		case preventSystemSleep
+		case declareUserActivity
+		case preventUserIdleSleep
+		case preventUserIdleSystemSleep
+
+		// Get the readable IOKit constant string for match
+		internal var ioKitAssertionType: String? {
+			switch self {
+			case .preventDisplaySleep: return kIOPMAssertionTypeNoDisplaySleep as String
+			case .preventSystemIdleSleep: return kIOPMAssertionTypeNoIdleSleep as String
+			case .preventSystemSleep: return kIOPMAssertionTypePreventSystemSleep as String
+			case .preventUserIdleSleep:
+				return kIOPMAssertionTypePreventUserIdleDisplaySleep as String
+			case .preventUserIdleSystemSleep:
+				return kIOPMAssertionTypePreventUserIdleSystemSleep as String
+			default: return nil
+			}
+		}
+
+		var description: String {
+			switch self {
+			case .preventDisplaySleep: return "preventDisplaySleep"
+			case .preventSystemIdleSleep: return "preventSystemIdleSleep"
+			case .preventSystemSleep: return "preventSystemSleep"
+			case .preventUserIdleSleep: return "preventUserIdleSleep"
+			case .preventUserIdleSystemSleep: return "preventUserIdleSystemSleep"
+			case .declareUserActivity: return "declareUserActivity"
+			}
+		}
+	}
+
