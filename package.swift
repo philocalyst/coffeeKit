9d4fbd417d14e94078a817d8ab4460d97abc286d
Miles Wirht <regime_skylark.0y@icloud.com>
2025-04-08 15:09:11 -0400

    Changed name to coffeeKit and removed clutter comments

--- package.swift
+++ package.swift
@@ -4,22 +4,18 @@
 import PackageDescription
 
 let package = Package(
-    name: "powerKit",
+    name: "coffeeKit",
     platforms: [.macOS(.v10_15)],
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
-            name: "powerKit",
-            targets: ["powerKit"])
+            name: "coffeeKit",
+            targets: ["coffeeKit"])
     ],
     dependencies: [.package(url: "https://github.com/apple/swift-log.git", from: "1.5.0")],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "powerKit",  // Your target name
+            name: "coffeeKit",
             dependencies: [
-                // Add Logging to your target's dependencies
                 .product(name: "Logging", package: "swift-log")
             ])
 
