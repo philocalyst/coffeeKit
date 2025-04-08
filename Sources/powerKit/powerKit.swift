b28a6ec8050aa3ff4cec1b8d9aa375217d98716d
Miles Wirht <regime_skylark.0y@icloud.com>
2025-04-08 14:59:34 -0400

    Import statements

---
+++ Sources/powerKit/powerKit.swift
@@ -0,0 +1,5 @@
+import Darwin  // For kqueue, kevent, pid_t, signal constants, pipe, fcntl, strerror, EPERM, EBADF, NOTE_EXIT, EVFILT_PROC, EVFILT_READ, EV_ADD, EV_ENABLE, EV_ONESHOT, O_NONBLOCK, F_GETFL, F_SETFL, EINTR
+import Foundation
+import IOKit.pwr_mgt  // For the vital Power Management headers
+import Logging
+
