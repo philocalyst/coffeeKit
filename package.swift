3f71c6d0840d777afd391e1396e92d073479b5a8
Miles Wirht <regime_skylark.0y@icloud.com>
2025-04-08 15:51:42 -0400

    Setup infastructure for future testing

--- package.swift
+++ package.swift
@@ -11,13 +11,23 @@ let package = Package(
             name: "coffeeKit",
             targets: ["coffeeKit"])
     ],
-    dependencies: [.package(url: "https://github.com/apple/swift-log.git", from: "1.5.0")],
+    dependencies: [
+        .package(url: "https://github.com/apple/swift-log.git", from: "1.5.0"),
+        .package(url: "https://github.com/apple/swift-testing.git", from: "0.7.0"),
+    ],
     targets: [
         .target(
             name: "coffeeKit",
             dependencies: [
                 .product(name: "Logging", package: "swift-log")
-            ])
-
+            ]
+        )
+        // .testTarget(
+        //     name: "coffeeKitTests",
+        //     dependencies: [
+        //         "coffeeKit",
+        //         .product(name: "Testing", package: "swift-testing"),
+        //     ]
+        // ),
     ]
 )
